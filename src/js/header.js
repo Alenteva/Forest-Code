bca8b60fd3e271a16bd04cdc5abd9557e1439ead
YouKnowWhoIs <leokatmax@gmail.com>
2024-03-02 21:31:14 +0200

    update

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,12 @@
 const switcher = document.querySelector('.header-switch');
 const bodyWallpaper = document.querySelector('body');
 const header = document.querySelector('.header');
-const svgElement = document.querySelector('.shop-list-basket');
+const svgElement1 = document.querySelector('.header-logo-svg');
+const svgElement2 = document.querySelector('.shop-list-svg-basket');
+const nav = document.querySelector('.header-nav');
 
 switcher.addEventListener('click', wallpaper);
+nav.addEventListener('click', headerSelect);
 
 function wallpaper() {
   if (switcher.checked) {
@@ -18,11 +21,21 @@ function darckWallpaper() {
   bodyWallpaper.style.color = '#ffffff';
   header.style.backgroundColor = '#111111';
   header.style.borderColor = '#ffffff';
+  svgElement1.innerHTML =
+    '<svg class="header-logo-svg" width="109" height="28"><use href="./images/icons/symbol-defs.svg#icon-logo2"></use></svg>';
+  svgElement2.innerHTML =
+    '<svg class="shop-list-svg-basket" width="14" height="18"><use href="./images/icons/symbol-defs.svg#icon-icon-basket2"></use></svg>';
 }
 
 function whiteWallpaper() {
-  bodyWallpaper.style.backgroundColor = '#b6b6b6';
+  bodyWallpaper.style.backgroundColor = '#F6F6F6';
   bodyWallpaper.style.color = '#111111';
   header.style.backgroundColor = '#ffffff';
   header.style.borderColor = '#111111';
+  svgElement1.innerHTML =
+    '<svg class="header-logo-svg" width="109" height="28"><use href="./images/icons/symbol-defs.svg#icon-logo"></use></svg>';
+  svgElement2.innerHTML =
+    '<svg class="shop-list-svg-basket" width="14" height="18"><use href="./images/icons/symbol-defs.svg#icon-icon-basket"></use></svg>';
 }
+
+function headerSelect() {}
