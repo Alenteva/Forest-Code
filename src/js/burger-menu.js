3913793273a5156a9fe2ac7b0d7b3d30e264ba43
KatyaKubar <116179784+KatyaKubar@users.noreply.github.com>
2024-03-02 22:26:57 +0200

    Summary

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -8,12 +8,12 @@ export function initBurgerMenu() {
     if (burgerMenu.classList.contains('burger-menu-show')) {
       burgerToggle.innerHTML = `
         <svg width="18" height="18" stroke="#000">
-          <use href="./images/icons/symbol-defs.svg#icon-close"></use>
+          <use href="/images/icons/symbol-defs.svg#icon-close"></use>
         </svg>`;
     } else {
       burgerToggle.innerHTML = `
         <svg width="28" height="28">
-          <use href="./images/icons/symbol-defs.svg#icon-menu"></use>
+          <use href="/images/icons/symbol-defs.svg#icon-menu"></use>
         </svg>`;
     }
   });
@@ -22,3 +22,15 @@ export function initBurgerMenu() {
 document.addEventListener('DOMContentLoaded', function () {
   initBurgerMenu();
 });
+
+const home = document.getElementById('home');
+const shoppingList = document.getElementById('shopping-list');
+
+home.classList.add('active');
+
+shoppingList.addEventListener('click', () => {
+  home.classList.remove('active');
+  shoppingList.classList.add('active');
+});
+
+
