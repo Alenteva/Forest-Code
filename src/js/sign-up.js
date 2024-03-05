b0fc75f3e0ddc83b3a4aebf7340f0e04c43cef65
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-03-05 17:34:45 +0200

    .

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -1,9 +1,37 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import {
+  connectAuthEmulator,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+// import { firebaseConfig } from './config';
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8UvisVnkCvMijmf6q4ZtLkQgC43vz2KM',
+  authDomain: 'dookshelf-357a4.firebaseapp.com',
+  projectId: 'dookshelf-357a4',
+  storageBucket: 'dookshelf-357a4.appspot.com',
+  messagingSenderId: '957443484270',
+  appId: '1:957443484270:web:de06245395594e6cf9947f',
+  measurementId: 'G-PGZE6HZC47',
+};
+initializeApp(firebaseConfig);
+const auth = getAuth();
 
 const formContainer = document.querySelector('.Form-window');
 const closeFormBtn = document.querySelector('.Close-form-btn');
 const btnSignUp = document.querySelector('.header-user');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const usernameInput = document.getElementById('username');
+const registBtn = document.querySelector('.form__btn');
+const signUp = document.querySelector('.form__btn-up');
+const btnOut = document.querySelector('.form__btn-in');
 
 btnSignUp.addEventListener('click', function () {
   formContainer.classList.add('is-open');
@@ -15,65 +43,4 @@ closeFormBtn.addEventListener('click', function () {
   //   document.body.style.overflow = '';
 });
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD8UvisVnkCvMijmf6q4ZtLkQgC43vz2KM',
-  authDomain: 'dookshelf-357a4.firebaseapp.com',
-  projectId: 'dookshelf-357a4',
-  storageBucket: 'dookshelf-357a4.appspot.com',
-  messagingSenderId: '957443484270',
-  appId: '1:957443484270:web:de06245395594e6cf9947f',
-  measurementId: 'G-PGZE6HZC47',
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const signUpButton = document.querySelector('.form__btn-up');
-// Додавання обробника подій для кнопки "Sign up"
-signUpButton.addEventListener('click', event => {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      // Успішно створено обліковий запис користувача
-      const user = userCredential.user;
-      btnSignUp.textContent = username;
-    })
-    .catch(error => {
-      // Обробка помилок під час реєстрації користувача
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error('Registration error:', errorMessage);
-    });
-});
-// ============================
-
-//  Реєстрація нового користувача
-firebase
-  .auth()
-  .createUserWithEmailAndPassword(email, password)
-  .then(userCredential => {
-    // Успішна реєстрація користувача
-    const user = userCredential.user;
-  })
-  .catch(error => {
-    // Обробка помилок реєстрації користувача
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-// Вхід користувача
-firebase
-  .auth()
-  .signInWithEmailAndPassword(email, password)
-  .then(userCredential => {
-    // Успішний вхід користувача
-    const user = userCredential.user;
-  })
-  .catch(error => {
-    // Обробка помилок входу користувача
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+// ========================================================================
