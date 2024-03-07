bdac847005baf4905f40f11b5ea2f10369debb28
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-03-07 16:38:02 +0000

    Deploying to gh-pages from @ Alenteva/Forest-Code@5e7b07fe446a81dc501ceec86e983f943e009713 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,13 +1,13 @@
-import"./assets/styles-ebea669e.js";document.addEventListener("DOMContentLoaded",function(){const c=document.querySelector(".marcup"),p=b(),g=document.querySelectorAll(".shoplist-btn-delete"),m=JSON.parse(localStorage.getItem("buy_links"))||[];p.length===0?c.innerHTML=d():(r(p),u());async function u(){g.forEach(o=>{o.addEventListener("click",async e=>{const a=document.querySelector("li");h(e),a.remove(a);const t=e.currentTarget.dataset.bookId,l=JSON.parse(localStorage.getItem("arrayBooksShop")).filter(i=>i._id!==t);localStorage.setItem("arrayBooksShop",JSON.stringify(l)),await new Promise(i=>setTimeout(i,1e3)),r(l)})})}function h(o){o.target.nodeName}function r(o){const e=o.slice(0,3);if(m.find(t=>t.name==="Amazon")){const t=e.map(l=>`<li>${f(l)}</li>`).join(""),n=document.querySelector(".marcup");n.innerHTML=`<ul>${t}</ul>`}else console.log("Ссылка на Amazon не найдена в localStorage.");document.querySelectorAll(".shoplist-btn-delete").forEach(t=>{t.addEventListener("click",k)})}function k(o){const a=o.currentTarget.dataset.bookId;let s=JSON.parse(localStorage.getItem("arrayBooksShop"));const t=s.findIndex(n=>n._id===a);t!==-1&&(s.splice(t,1),localStorage.setItem("arrayBooksShop",JSON.stringify(s)),r(s),S())}function b(){const o=localStorage.getItem("arrayBooksShop")||"[]";return JSON.parse(o)}function S(){const o=JSON.parse(localStorage.getItem("arrayBooksShop"));(!o||o.length===0)&&(c.innerHTML=d())}function f(o){const{_id:e,book_image:a,title:s,list_name:t,description:n,author:l,buy_links:i}=o;return`
+import"./assets/styles-02ccdce5.js";document.addEventListener("DOMContentLoaded",function(){const p=document.querySelector(".marcup"),r=h();console.log(r);const g=document.querySelectorAll(".shoplist-btn-delete");JSON.parse(localStorage.getItem("buy_links")),r.length===0?p.innerHTML=d():(c(r),u());async function u(){g.forEach(o=>{o.addEventListener("click",async e=>{const l=o.querySelector("li");m(e),l.remove();const a=e.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("arrayBooksShop")).filter(n=>n._id!==a);localStorage.setItem("arrayBooksShop",JSON.stringify(i)),await new Promise(n=>setTimeout(n,1e3)),c(i)})})}function m(o){o.target.nodeName}function c(o){const e=o.slice(0,3);if((JSON.parse(localStorage.getItem("buy_links"))||[]).find(t=>t.name==="Amazon")){const t=e.map(n=>`<li>${S(n)}</li>`).join(""),i=document.querySelector(".marcup");i.innerHTML=`<ul>${t}</ul>`}else console.log("Ссылка на Amazon не найдена в localStorage.");document.querySelectorAll(".shoplist-btn-delete").forEach(t=>{t.addEventListener("click",k)})}function k(o){const l=o.currentTarget.dataset.bookId;let s=JSON.parse(localStorage.getItem("arrayBooksShop"));const a=s.findIndex(t=>t._id===l);a!==-1&&(s.splice(a,1),localStorage.setItem("arrayBooksShop",JSON.stringify(s)),c(s),b())}function h(){const o=localStorage.getItem("arrayBooksShop")||"[]";return JSON.parse(o)}function b(){const o=JSON.parse(localStorage.getItem("arrayBooksShop"));(!o||o.length===0)&&(p.innerHTML=d())}function S(o){const{_id:e,book_image:l,title:s,list_name:a,description:t,author:i,buy_links:n}=o;return`
    <div class="shoplist-error container">
 
       <div id="${e}"></div>
-        <img src="${a}" alt="${s}" class="shoplist-bookcover"/>
+        <img src="${l}" alt="${s}" class="shoplist-bookcover"/>
         <div class="shoplist">
           <h2 class="shoplist-title-book">${s}</h2>
-          <p class="shoplist-category-book">${t}</p>
-          <p class="shoplist-description">${n}</p>
-          <p class="shoplist-author">${l}</p>
+          <p class="shoplist-category-book">${a}</p>
+          <p class="shoplist-description">${t}</p>
+          <p class="shoplist-author">${i}</p>
         </div>
          <div class="logo-book">
         <button id="deleteBtn-${e}" type="button" class="shoplist-btn-delete" data-book-id="${e}">
@@ -16,7 +16,7 @@ import"./assets/styles-ebea669e.js";document.addEventListener("DOMContentLoaded"
           </svg>
         </button>
                <ul class="shoplist-links">
-  <li class="shoplist-item"><a href="${i.find(v=>v.name==="Amazon").url}" target="_blank"><img src="../images/shopping-list/amazon.png" alt="amazon logo" class="amazon"></a></li>
+  <li class="shoplist-item"><a href="${n.find(f=>f.name==="Amazon").url}" target="_blank"><img src="../images/shopping-list/amazon.png" alt="amazon logo" class="amazon"></a></li>
           <li><img src="../images/shopping-list/book.png" alt="open book" class="apple-book"></li>
         </ul>
         </div>
