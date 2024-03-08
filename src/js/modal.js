56721e94fac36b507e4e2213bfe159d3f4aff75d
Vadym <vadymlysytskyi@gmail.com>
2024-03-08 18:10:20 +0100

    fix photos (possible)

--- src/js/modal.js
+++ src/js/modal.js
@@ -39,8 +39,8 @@ async function renderBook(_id) {
               <p class="book-author">${book.author}</p>
               <p class="modal-book-description">${book.description}</p>
               <div class="links-books">
-                  <a class="" href="${book.buy_links[0].url}" rel="amazon ${book.title}" target="_blank""><img src="./images/myPhotos/amazon.png" alt="amazon" class="filter-img"></a>
-                  <a class="" href="${book.buy_links[1].url}" rel="apple-book" target="_blank""><img src="./images/myPhotos/book.png" alt="amazon" class="filter-img"></a>
+                  <a class="" href="${book.buy_links[0].url}" rel="amazon ${book.title}" target="_blank"><img src="./images/myPhotos/amazon.png" alt="amazon" class="filter-img"></a>
+                  <a class="" href="${book.buy_links[1].url}" rel="apple-book" target="_blank"><img src="./images/myPhotos/book.png" alt="apple-book" class="filter-img"></a>
               </div>
           </div>
       </div>
