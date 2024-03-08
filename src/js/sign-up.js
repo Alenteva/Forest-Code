f45fb2484e17942526e24bccce202e8266b8458e
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-03-08 16:17:33 +0200

    .

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -1,3 +1,304 @@
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import {
+//   connectAuthEmulator,
+//   createUserWithEmailAndPassword,
+//   getAuth,
+//   onAuthStateChanged,
+//   sendEmailVerification,
+//   sendPasswordResetEmail,
+//   signInWithEmailAndPassword,
+//   signInWithCustomToken,
+//   signOut,
+//   updateProfile,
+// } from 'firebase/auth';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyD8UvisVnkCvMijmf6q4ZtLkQgC43vz2KM',
+//   authDomain: 'dookshelf-357a4.firebaseapp.com',
+//   projectId: 'dookshelf-357a4',
+//   storageBucket: 'dookshelf-357a4.appspot.com',
+//   messagingSenderId: '957443484270',
+//   appId: '1:957443484270:web:de06245395594e6cf9947f',
+//   measurementId: 'G-PGZE6HZC47',
+// };
+// initializeApp(firebaseConfig);
+// const auth = getAuth();
+
+// onAuthStateChanged(auth, async user => {
+//   if (user) {
+//     formContainer.classList.remove('is-open');
+//     headerNav.style.display = 'flex';
+//     iziToast.show({
+//       title: 'Hello',
+//       message: `Welcome, ${user.displayName}!`,
+//       color: 'blue',
+//       position: 'topCenter',
+//     });
+//     headerSignUp.textContent = user.displayName;
+//     burgSignUp.textContent = user.displayName;
+//     localStorage.setItem(
+//       'user-data',
+//       JSON.stringify({
+//         name: user.displayName,
+//         mail: user.email,
+//       })
+//     );
+//   } else {
+//     headerNav.style.display = 'none';
+//     burgMenu.style.display = 'none';
+//     formContainer.classList.remove('is-open');
+//     // headerLogOut.style.display = 'none';
+//   }
+// });
+
+// const formContainer = document.querySelector('.Form-window');
+// const closeFormBtn = document.querySelector('.Close-form-btn');
+// const headerSignUp = document.querySelectorAll('.sign-up');
+// const burgSignUp = document.querySelector('.burger-menu-user');
+// const emailInput = document.getElementById('email');
+// const passwordInput = document.getElementById('password');
+// const usernamelInput = document.getElementById('username');
+// const signInButton = document.querySelector('.quickstart-sign-in');
+// const signUpButton = document.querySelector('.quickstart-sign-up');
+// const headerNav = document.querySelector('.header-nav');
+// const burgMenu = document.querySelector('.burger-menu-list');
+// const passwordResetButton = document.querySelector('.reset-btn');
+// const logOutBtn = document.querySelector('.log-out-btn');
+// const headerLogOut = document.querySelector('.Log-out-btn');
+// // ========================================================================
+
+// // ========================================================================
+// headerSignUp.forEach(el => {
+//   el.addEventListener('click', function () {
+//     formContainer.classList.add('is-open');
+//   });
+// });
+// closeFormBtn.addEventListener('click', function () {
+//   formContainer.classList.remove('is-open');
+// });
+// // =================================================
+// function toggleSignIn() {
+//   if (auth.currentUser) {
+//     signOut(auth).then(() => {
+//       handleSignOut();
+//       updateUI();
+//     });
+//   } else {
+//     const email = emailInput.value;
+//     const password = passwordInput.value;
+//     const name = usernamelInput.value;
+
+//     if (email.length < 4) {
+//       alert('Please enter an email address');
+//       return;
+//     }
+
+//     if (password.length < 4) {
+//       alert('Please enter a password with at least 4 characters');
+//       return;
+//     }
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then(userCredential => {
+//         const user = userCredential.user;
+//         user.displayName = name;
+//         formContainer.classList.remove('is-open');
+//         headerNav.style.display = 'flex';
+//         burgMenu.style.display = 'block';
+//         headerSignUp.textContent = user.displayName;
+//         burgSignUp.textContent = user.displayName;
+//         iziToast.show({
+//           title: 'Hello',
+//           message: `Welcome, ${user.displayName}!`,
+//           color: 'blue',
+//           position: 'topCenter',
+//         });
+//         localStorage.setItem(
+//           'user-data',
+//           JSON.stringify({
+//             name: user.displayName,
+//             mail: user.email,
+//           })
+//         );
+//         user.getIdToken().then(token => {
+//           localStorage.setItem('userToken', token);
+//         });
+//       })
+//       .catch(error => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+
+//         if (errorCode === 'auth/wrong-password') {
+//           alert('Wrong password');
+//         } else {
+//           alert(errorMessage);
+//         }
+//         console.error(error);
+//       });
+//   }
+// }
+// // ===============================================
+// function handleSignUp() {
+//   const email = emailInput.value;
+//   const password = passwordInput.value;
+//   const name = usernamelInput.value;
+//   if (email.length < 4) {
+//     alert('Please enter an email address.');
+//     return;
+//   }
+//   if (password.length < 4) {
+//     alert('Please enter a password.');
+//     return;
+//   }
+//   // Create user with email and pass.
+//   createUserWithEmailAndPassword(auth, email, password)
+//     .then(async userCredential => {
+//       const user = userCredential.user;
+//       user.displayName = name;
+//       formContainer.classList.remove('is-open');
+//       headerNav.style.display = 'flex';
+//       burgMenu.style.display = 'block';
+//       headerSignUp.textContent = user.displayName;
+//       burgSignUp.textContent = user.displayName;
+//       // burgSignUp.textContent = user.displayName;
+//       await updateProfile(user, { displayName: user.displayName });
+//       iziToast.show({
+//         title: 'Ok',
+//         message: 'You have successfully registered!',
+//         color: 'blue',
+//         position: 'topCenter',
+//       });
+//       localStorage.setItem(
+//         'user-data',
+//         JSON.stringify({
+//           name: user.displayName,
+//           mail: user.email,
+//         })
+//       );
+//       user.getIdToken().then(token => {
+//         localStorage.setItem('userToken', token);
+//       });
+//     })
+//     .catch(function (error) {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       if (errorCode == 'auth/weak-password') {
+//         alert('The password is too weak.');
+//       } else {
+//         alert(errorMessage);
+//       }
+//       console.log(error);
+//     });
+// }
+// // =========================================================================
+// window.onload = function () {
+//   const token = localStorage.getItem('userToken');
+//   if (token) {
+//     signInWithCustomToken(auth, token)
+//       .then(userCredential => {
+//         const user = userCredential.user;
+//         const userData = JSON.parse(localStorage.getItem('user-data'));
+//         if (userData) {
+//           user.displayName = userData.name;
+//           user.email = userData.mail;
+//           formContainer.classList.remove('is-open');
+//           headerNav.style.display = 'flex';
+//           burgMenu.style.display = 'block';
+//           headerSignUp.forEach(el => {
+//             el.textContent = user.displayName;
+//           });
+//           headerSignUp.textContent = user.displayName;
+//           burgSignUp.textContent = user.displayName;
+//           updateUI(user);
+//         }
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   } else {
+//     updateUI();
+//   }
+// };
+
+// function updateUI(user) {
+//   if (user) {
+//     // Відображення UI для автентифікованого користувача
+//     formContainer.classList.remove('is-open');
+//     headerNav.style.display = 'flex';
+//     burgMenu.style.display = 'block';
+//     headerLogOut.style.display = 'inline-block';
+//     headerSignUp.textContent = user.displayName;
+//     burgSignUp.textContent = user.displayName;
+//   } else {
+//     // Відображення UI для неавтентифікованого користувача
+//     headerNav.style.display = 'none';
+//     burgMenu.style.display = 'none';
+//     formContainer.classList.remove('is-open');
+//     headerLogOut.style.display = 'none';
+//   }
+// }
+// // ========================================================================
+// function resetPassword() {
+//   const email = emailInput.value;
+//   sendPasswordResetEmail(auth, email)
+//     .then(function () {
+//       iziToast.show({
+//         message: 'Password reset email sent. Check your email',
+//         color: 'yellow',
+//         position: 'topCenter',
+//       });
+//     })
+//     .catch(function (error) {
+//       // Handle Errors here.
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       if (errorCode == 'auth/invalid-email') {
+//         alert(errorMessage);
+//       } else if (errorCode == 'auth/user-not-found') {
+//         alert(errorMessage);
+//       }
+//       console.log(error);
+//     });
+// }
+// // ========================================================================
+// function handleSignOut() {
+//   if (auth.currentUser) {
+//     signOut(auth).then(() => {
+//       localStorage.removeItem('user-data'); // Очищення кешованих даних користувача
+//       localStorage.removeItem('userToken'); // Видалення токена після виходу
+//       updateUI(); // Оновлення UI після виходу
+//     });
+//   }
+// }
+// // ========================================================================
+// signUpButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   handleSignUp();
+// });
+
+// signInButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   toggleSignIn();
+// });
+
+// passwordResetButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   resetPassword();
+// });
+
+// logOutBtn.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   handleSignOut();
+// });
+// headerLogOut.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   handleSignOut();
+// });
+// ======================================================================
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -13,6 +314,7 @@ import {
   signInWithEmailAndPassword,
   signInWithCustomToken,
   signOut,
+  updateProfile,
 } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -27,26 +329,58 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const auth = getAuth();
 
+onAuthStateChanged(auth, async user => {
+  if (user) {
+    console.log('user', user);
+    formContainer.classList.remove('is-open');
+    headerNav.style.display = 'flex';
+    iziToast.show({
+      title: 'Hello',
+      message: `Welcome, ${user.displayName}!`,
+      color: 'blue',
+      position: 'topCenter',
+    });
+    headerSignUp.forEach(el => (el.textContent = user.displayName));
+    // document.querySelector('#signupHeader').textContent = user.displayName;
+    localStorage.setItem(
+      'user-data',
+      JSON.stringify({
+        name: user.displayName,
+        mail: user.email,
+      })
+    );
+  } else {
+    // Користувач вийшов з системи
+    // ...
+  }
+});
 const formContainer = document.querySelector('.Form-window');
 const closeFormBtn = document.querySelector('.Close-form-btn');
 const headerSignUp = document.querySelectorAll('.sign-up');
-const burgSignUp = document.querySelector('.burger-menu-user');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const usernamelInput = document.getElementById('username');
 const signInButton = document.querySelector('.quickstart-sign-in');
 const signUpButton = document.querySelector('.quickstart-sign-up');
 const headerNav = document.querySelector('.header-nav');
-const burgMenu = document.querySelector('.burger-menu-list');
 const passwordResetButton = document.querySelector('.reset-btn');
 const logOutBtn = document.querySelector('.log-out-btn');
-const headerLogOut = document.querySelector('.Log-out-btn');
 // ========================================================================
+// const signupHeaderBtn = document.getElementById('signupHeader');
+// const signupBurgerBtn = document.getElementById('signupBurger');
+// function handleButtonClick(event) {
+//   formContainer.classList.add('is-open');
+// }
+// signupHeaderBtn.addEventListener('click', handleButtonClick);
+// signupBurgerBtn.addEventListener('click', handleButtonClick);
+
 headerSignUp.forEach(el => {
   el.addEventListener('click', function () {
     formContainer.classList.add('is-open');
+    //   document.body.style.overflow = 'hidden';
   });
 });
+
 closeFormBtn.addEventListener('click', function () {
   formContainer.classList.remove('is-open');
 });
@@ -71,15 +405,14 @@ function toggleSignIn() {
       alert('Please enter a password with at least 4 characters');
       return;
     }
+
     signInWithEmailAndPassword(auth, email, password)
       .then(userCredential => {
         const user = userCredential.user;
         user.displayName = name;
         formContainer.classList.remove('is-open');
         headerNav.style.display = 'flex';
-        burgMenu.style.display = 'block';
         headerSignUp.textContent = user.displayName;
-        burgSignUp.textContent = user.displayName;
         iziToast.show({
           title: 'Hello',
           message: `Welcome, ${user.displayName}!`,
@@ -125,14 +458,13 @@ function handleSignUp() {
   }
   // Create user with email and pass.
   createUserWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
+    .then(async userCredential => {
       const user = userCredential.user;
       user.displayName = name;
       formContainer.classList.remove('is-open');
       headerNav.style.display = 'flex';
-      burgMenu.style.display = 'block';
-      headerSignUp.textContent = user.displayName;
-      burgSignUp.textContent = user.displayName;
+      document.querySelector('#signupHeader').textContent = user.displayName;
+      await updateProfile(user, { displayName: user.displayName });
       iziToast.show({
         title: 'Ok',
         message: 'You have successfully registered!',
@@ -162,47 +494,40 @@ function handleSignUp() {
     });
 }
 // =========================================================================
-window.onload = function () {
-  const token = localStorage.getItem('userToken');
-  if (token) {
-    signInWithCustomToken(auth, token)
-      .then(userCredential => {
-        const user = userCredential.user;
-        const userData = JSON.parse(localStorage.getItem('user-data'));
-        if (userData) {
-          user.displayName = userData.name;
-          user.email = userData.mail;
-          formContainer.classList.remove('is-open');
-          headerNav.style.display = 'flex';
-          burgMenu.style.display = 'block';
-          headerSignUp.textContent = user.displayName;
-          burgSignUp.textContent = user.displayName;
-          updateUI(user);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  } else {
-    updateUI();
-  }
-};
+// window.onload = function () {
+//   const token = localStorage.getItem('userToken');
+//   if (token) {
+//     signInWithCustomToken(auth, token)
+//       .then(userCredential => {
+//         const user = userCredential.user;
+//         const userData = JSON.parse(localStorage.getItem('user-data'));
+//         if (userData) {
+//           user.displayName = userData.name;
+//           user.email = userData.mail;
+//           formContainer.classList.remove('is-open');
+//           headerNav.style.display = 'flex';
+//           headerSignUp.textContent = user.displayName;
+//           updateUI(user);
+//         }
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   } else {
+//     updateUI();
+//   }
+// };
 
 function updateUI(user) {
   if (user) {
     // Відображення UI для автентифікованого користувача
     formContainer.classList.remove('is-open');
     headerNav.style.display = 'flex';
-    burgMenu.style.display = 'block';
-    headerLogOut.style.display = 'inline-block';
     headerSignUp.textContent = user.displayName;
-    burgSignUp.textContent = user.displayName;
   } else {
     // Відображення UI для неавтентифікованого користувача
     headerNav.style.display = 'none';
-    burgMenu.style.display = 'none';
     formContainer.classList.remove('is-open');
-    headerLogOut.style.display = 'none';
   }
 }
 // ========================================================================
@@ -241,6 +566,7 @@ function handleSignOut() {
 // ========================================================================
 signUpButton.addEventListener('click', function (event) {
   event.preventDefault();
+  formContainer.classList.add('is-open');
   handleSignUp();
 });
 
@@ -258,8 +584,3 @@ logOutBtn.addEventListener('click', function (event) {
   event.preventDefault();
   handleSignOut();
 });
-headerLogOut.addEventListener('click', function (event) {
-  event.preventDefault();
-  handleSignOut();
-});
-// ======================================================================
